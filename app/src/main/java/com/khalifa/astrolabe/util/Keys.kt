05f9d246f33580708a201323656e6412bbbfaeba
AhmadKhalifa <ahmad.khalifa795@gmail.com>
2018-11-22 17:42:12 +0200

    Added transparency controller

---
+++ app/src/main/java/com/khalifa/astrolabe/util/Keys.kt
@@ -0,0 +1,24 @@
+package com.khalifa.astrolabe.util
+
+/**
+ * @author Ahmad Khalifa
+ */
+
+object Keys {
+    object Bing {
+        const val API_KEY = "AtRgTpQChfULNPfAppNlczeIZFzpjqhZ9tsjWtWg0dqUkdQzVmLHe6q2sekEE7Kw"
+    }
+
+    object MapBox {
+        const val ACCESS_TOKEN = "sk.eyJ1IjoiZGV2b3JjIiwiYSI6ImNqb2U0ZXM2aDFrOTAzcW9qbDAzNTVxdTcifQ.hXYhbYFZaIQygWHmu72XkQ"
+    }
+
+    object HereWeGo {
+        const val APP_ID = "NObLN4n6HA2PCIhQn2o8"
+        const val APP_CODE = "gs7fWM3zpfmUTeyClNirQg"
+    }
+
+    object ThunderForest {
+        const val API_KEY = "ea62a886daae45ae811a3e140376f5b8"
+    }
+}
